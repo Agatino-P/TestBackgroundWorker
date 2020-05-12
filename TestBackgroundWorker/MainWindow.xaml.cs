33c30b40a5bafe91f20cd07e6129ad2c76d8ad79
agatino.work@gmail.com <agatino.work@gmail.com>
2020-05-12 13:55:58 +0200

    Add project files.

---
+++ TestBackgroundWorker/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestBackgroundWorker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+
+        public int Percentage
+        {
+            get { return (int)GetValue(PercentageProperty); }
+            set { SetValue(PercentageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Percentage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PercentageProperty =
+            DependencyProperty.Register("Percentage", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+     
+    }
+}
---
+++ TestBackgroundWorker/MainWindowViewModel.cs
@@ -0,0 +1,93 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+
+namespace TestBackgroundWorker
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private int _percentage; public int Percentage { get => _percentage; set { Set(() => Percentage, ref _percentage, value); } }
+        private int _max = 100; public int Max { get => _max; set { Set(() => Max, ref _max, value); } }
+        private string _counter; public string Counter { get => _counter; set { Set(() => Counter, ref _counter, value); }}
+
+        BackgroundWorker worker = new BackgroundWorker() { WorkerReportsProgress = true, WorkerSupportsCancellation = true };
+
+        private RelayCommand _startCmd;
+        public RelayCommand StartCmd => _startCmd ?? (_startCmd = new RelayCommand(
+            () => start(),
+            () => { return 1 == 1; },
+            keepTargetAlive: true
+            ));
+
+        private void start()
+        {
+            Percentage = 0;
+            worker.RunWorkerAsync(Max);
+        }
+
+
+        private RelayCommand _stopCmd;
+        public RelayCommand StopCmd => _stopCmd ?? (_stopCmd = new RelayCommand(
+            () => Stop(),
+            () => { return 1 == 1; },
+			keepTargetAlive:true
+            ));
+
+        private void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (!(sender is BackgroundWorker worker))
+                return;
+
+            if (!(e.Argument is int max))
+                return;
+            for (int  counter=1; counter <=max; counter++)
+            {
+                Thread.Sleep(100);
+                int percentage = (counter * 100) / max;
+                worker.ReportProgress(percentage, counter);
+            }
+            e.Result = Max;
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (!(sender is BackgroundWorker worker))
+                return;
+            if (!(e.UserState is int counter))
+                return;
+
+            Percentage = e.ProgressPercentage;
+            Counter = counter.ToString();
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!(e.Result is int result))
+                return;
+
+            Percentage = 100;
+            Counter = result.ToString();
+        }
+
+        public MainWindowViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                Percentage = 30;
+            }
+
+            worker.DoWork += Worker_DoWork;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+        }
+    }
+}
